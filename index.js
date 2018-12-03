b9e1db66e11860c41db6c0e069ee09385d62ff2c
Vinh Tran <vinhjs@gmail.com>
2018-12-03 17:40:11 +0700

    cache to mongodb

--- index.js
+++ index.js
@@ -113,158 +113,148 @@ app.get('/sample', authMiddleware, function(req, res){
         }
     } 
     var startAt = 0;
-    var total = 50  
-    async.whilst(
-        function () { return startAt < total; },
-        function (callback) {
-            request({
-                url: 'https://issues.qup.vn/rest/api/2/search?startAt='+startAt+'&jql=Sprint in (291,292,293)',
-                timeout: 10000,
-                json: true,
-                headers: {
-                    "Authorization": req.headers.authorization
-                }
-            }, function(error, response, result){
-                if (result && result.total) {                                           
-                    total = result.total;
-                    startAt += 50;
-                    async.forEachLimit(result.issues, 10, function(issue, cback){ 
-                        var issuetype = _.get(issue, "fields.issuetype.name", "unknown");
-                        var issuestatus = _.get(issue, "fields.status.name", "unknown");
-                        finish.issuetypes = _.uniq(finish.issuetypes.push(issuetype));
-                        var components = _.get(issue, "fields.components", []);
-                        if (issue.fields.subtasks.length == 0) {
-                            components.forEach(function(component){
-                                if (finish.components[component.name]) {
-                                    finish.components[component.name].total++;
-                                    if (finish.components[component.name][issuestatus]) {
-                                        finish.components[component.name][issuestatus]++;
-                                    } else {
-                                        finish.components[component.name][issuestatus] = 1;
-                                    }
-                                } else {
-                                    finish.components[component.name] = {
-                                        total: 1
-                                    }
-                                    finish.components[component.name][issuestatus] = 1;
+    var total = 100;
+    var jql= "Sprint in (293)";
+    jira_utils.getResults(jql, function(err, data){
+        if (data) {
+            data.data = JSON.parse(data.data);
+            res.send(data);
+        } else {
+            async.whilst(
+                function () { return startAt < total; },
+                function (callback) {
+                    request({
+                        url: 'https://issues.qup.vn/rest/api/2/search?maxResults=100&startAt='+startAt+'&jql=' + jql,
+                        timeout: 10000,
+                        json: true,
+                        headers: {
+                            "Authorization": req.headers.authorization
+                        }
+                    }, function(error, response, result){
+                        if (result && result.total) {                                           
+                            total = result.total;
+                            startAt += 100;
+                            async.forEachLimit(result.issues, 10, function(issue, cback){ 
+                                var issuetype = _.get(issue, "fields.issuetype.name", "unknown");
+                                var issuestatus = _.get(issue, "fields.status.name", "unknown");
+                                finish.issuetypes = _.uniq(finish.issuetypes.push(issuetype));
+                                var components = _.get(issue, "fields.components", []);
+                                if (issue.fields.subtasks.length == 0) {
+                                    components.forEach(function(component){
+                                        if (finish.components[component.name]) {
+                                            finish.components[component.name].total++;
+                                            if (finish.components[component.name][issuestatus]) {
+                                                finish.components[component.name][issuestatus]++;
+                                            } else {
+                                                finish.components[component.name][issuestatus] = 1;
+                                            }
+                                        } else {
+                                            finish.components[component.name] = {
+                                                total: 1
+                                            }
+                                            finish.components[component.name][issuestatus] = 1;
+                                        }
+                                    })
                                 }
-                            })
-                        }
-                        if (issuetype === "Story") {
-                            finish.point += _.get(issue, "fields.customfield_10004", 0);
-                        }
-                        if(finish.issues.status[issuestatus]) {
-                            finish.issues.status[issuestatus]++;
-                        } else {
-                            finish.issues.status[issuestatus] = 1;
-                        }
-                        finish.issues.total++;
-                        finish.issues[issuetype] = finish.issues[issuetype] ? finish.issues[issuetype] + 1 : 1;
-                        //get log work info
-                        jira_utils.getWorklog(issue.key, req.headers.authorization, function(err, rs){
-                            if (rs && rs.length) {
-                                rs.forEach(function(worklog){
-                                    var dateCreated = new moment(worklog.created).format('YYYY-MM-DD');
-                                    finish.users[worklog.name] = finish.users[worklog.name] || {total: 0};
-                                    finish.users[worklog.name].total += worklog.timeSpentSeconds;
-                                    finish.users[worklog.name][issuetype] = finish.users[worklog.name][issuetype] || 0;
-                                    finish.users[worklog.name][issuetype] += worklog.timeSpentSeconds;
-                                    
-                                    //barchart         
-                                    finish.barChartLogworkData.labels.push(worklog.name)
-                                    var users = _.uniq(finish.barChartLogworkData.labels);                                    
-                                    finish.barChartLogworkData.labels = users;
-                                    var labelIndex = _.indexOf(users, worklog.name);
-                                    var datasetIndex = _.findIndex(finish.barChartLogworkData.datasets, function(o) { return o.label == issuetype; });
-                                    if (datasetIndex == -1) {
-                                        finish.barChartLogworkData.datasets.push({
-                                            label: issuetype,
-                                            backgroundColor: colors.issuetypes[finish.barChartLogworkData.datasets.length].code,
-                                            data: []
-                                        });  
-                                        datasetIndex =  _.findIndex(finish.barChartLogworkData.datasets, function(o) { return o.label == issuetype; });                                    
+                                if (issuetype === "Story") {
+                                    finish.point += _.get(issue, "fields.customfield_10004", 0);
+                                }
+                                if(finish.issues.status[issuestatus]) {
+                                    finish.issues.status[issuestatus]++;
+                                } else {
+                                    finish.issues.status[issuestatus] = 1;
+                                }
+                                finish.issues.total++;
+                                finish.issues[issuetype] = finish.issues[issuetype] ? finish.issues[issuetype] + 1 : 1;
+                                //get log work info
+                                jira_utils.getWorklog(issue.key, req.headers.authorization, function(err, rs){
+                                    if (rs && rs.length) {
+                                        rs.forEach(function(worklog){
+                                            var dateCreated = new moment(worklog.created).format('YYYY-MM-DD');
+                                            finish.users[worklog.name] = finish.users[worklog.name] || {total: 0};
+                                            finish.users[worklog.name].total += worklog.timeSpentSeconds;
+                                            finish.users[worklog.name][issuetype] = finish.users[worklog.name][issuetype] || 0;
+                                            finish.users[worklog.name][issuetype] += worklog.timeSpentSeconds;
+                                            
+                                            //barchart         
+                                            finish.barChartLogworkData.labels.push(worklog.name)
+                                            var users = _.uniq(finish.barChartLogworkData.labels);                                    
+                                            finish.barChartLogworkData.labels = users;
+                                            var labelIndex = _.indexOf(users, worklog.name);
+                                            var datasetIndex = _.findIndex(finish.barChartLogworkData.datasets, function(o) { return o.label == issuetype; });
+                                            if (datasetIndex == -1) {
+                                                finish.barChartLogworkData.datasets.push({
+                                                    label: issuetype,
+                                                    backgroundColor: colors.issuetypes[finish.barChartLogworkData.datasets.length].code,
+                                                    data: []
+                                                });  
+                                                datasetIndex =  _.findIndex(finish.barChartLogworkData.datasets, function(o) { return o.label == issuetype; });                                    
+                                            }
+                                            if(!finish.barChartLogworkData.datasets[datasetIndex].data[labelIndex]) {
+                                                finish.barChartLogworkData.datasets[datasetIndex].data[labelIndex] = 0;
+                                            }
+                                            finish.barChartLogworkData.datasets[datasetIndex].data[labelIndex] += worklog.timeSpentSeconds;
+        
+                                            finish.dates[dateCreated] = finish.dates[dateCreated] || {};
+                                            finish.dates[dateCreated][worklog.name] = finish.dates[dateCreated][worklog.name] || {total: 0};
+                                            finish.dates[dateCreated][worklog.name].total += worklog.timeSpentSeconds;
+                                            finish.dates[dateCreated][worklog.name][issuetype] = finish.dates[dateCreated][worklog.name][issuetype] || 0;
+                                            finish.dates[dateCreated][worklog.name][issuetype] += worklog.timeSpentSeconds;
+                                        })
                                     }
-                                    if(!finish.barChartLogworkData.datasets[datasetIndex].data[labelIndex]) {
-                                        finish.barChartLogworkData.datasets[datasetIndex].data[labelIndex] = 0;
-                                    }
-                                    finish.barChartLogworkData.datasets[datasetIndex].data[labelIndex] += worklog.timeSpentSeconds;
-
-                                    finish.dates[dateCreated] = finish.dates[dateCreated] || {};
-                                    finish.dates[dateCreated][worklog.name] = finish.dates[dateCreated][worklog.name] || {total: 0};
-                                    finish.dates[dateCreated][worklog.name].total += worklog.timeSpentSeconds;
-                                    finish.dates[dateCreated][worklog.name][issuetype] = finish.dates[dateCreated][worklog.name][issuetype] || 0;
-                                    finish.dates[dateCreated][worklog.name][issuetype] += worklog.timeSpentSeconds;
+                                    console.log(finish.issues.total +"/"+ result.total)
+                                    cback();
                                 })
-                            }
-                            console.log(finish.issues.total +"/"+ result.total)
-                            cback();
-                        })
-                    }, function(){
-                        callback();
-                    })
-                } else {
-                    callback(error || response.statusCode);
-                }
-            })
-        },
-        function (err, n) {
-            if (err) {
-                console.log(err);
-            }
-            console.log('DONE');
-            //remove null value in datasets.data
-            finish.barChartLogworkData.datasets = _.map(finish.barChartLogworkData.datasets, function(dataset) { 
-                return {
-                    label: dataset.label,
-                    backgroundColor: dataset.backgroundColor,
-                    data: _.map(dataset.data, function(dts) { 
-                        if (!dts) {
-                            return 0;
+                            }, function(){
+                                callback();
+                            })
                         } else {
-                            return (dts/60/60);
-                        }                        
+                            callback(error || response.statusCode);
+                        }
                     })
-                }                
-            });
-            var components = _.keys(finish.components);
-            var issuestatus = _.keys(finish.issues.status);
-            var datasets = [];  
-            for(var i in issuestatus) {
-                datasets.push({
-                    "label": issuestatus[i],
-                    "backgroundColor": colors.issuestatus[i].code || "rgb(255, 99, 132)",
-                    "data": _.map(components, function(component){
-                        return finish.components[component][issuestatus[i]] || 0;
-                    })
-                })
-            }          
-            finish.barChartComponentData = {
-                labels: components,
-                datasets: datasets
-            } 
-            console.log(JSON.stringify(finish));
+                },
+                function (err, n) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    console.log('DONE');
+                    //remove null value in datasets.data
+                    finish.barChartLogworkData.datasets = _.map(finish.barChartLogworkData.datasets, function(dataset) { 
+                        return {
+                            label: dataset.label,
+                            backgroundColor: dataset.backgroundColor,
+                            data: _.map(dataset.data, function(dts) { 
+                                if (!dts) {
+                                    return 0;
+                                } else {
+                                    return (dts/60/60);
+                                }                        
+                            })
+                        }                
+                    });
+                    var components = _.keys(finish.components);
+                    var issuestatus = _.keys(finish.issues.status);
+                    var datasets = [];  
+                    for(var i in issuestatus) {
+                        datasets.push({
+                            "label": issuestatus[i],
+                            "backgroundColor": colors.issuestatus[i].code || "rgb(255, 99, 132)",
+                            "data": _.map(components, function(component){
+                                return finish.components[component][issuestatus[i]] || 0;
+                            })
+                        });
+                    }          
+                    finish.barChartComponentData = {
+                        labels: components,
+                        datasets: datasets
+                    } 
+                    jira_utils.setResults(jql, finish);
+                    res.send(finish);
+                }
+            );            
         }
-    );
-    var barChartLogworkData = [[
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor()
-            ],[
-                randomScalingFactor50(),
-                randomScalingFactor50(),
-                randomScalingFactor50(),
-                randomScalingFactor50(),
-                randomScalingFactor50(),
-                randomScalingFactor50(),
-                randomScalingFactor50()
-            ]
-        ];
-    res.send(barChartLogworkData);
-})
+    });    
+});
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/full.html');
 })
--- jira_utils.js
+++ jira_utils.js
@@ -1,12 +1,27 @@
 var request = require('request');
 var _ = require('lodash');
-var redis = require('./redis');
+var MongoClient = require('mongodb').MongoClient;
+var url      = 'mongodb://192.168.2.85:27017';
+var mongodb = null;
+function connectMongo(){
+    MongoClient.connect(url, function(err, client) {
+        if (err) {
+            console.log(err);
+        } 
+        if (client) {
+            console.log("MongoDb connected");
+        }
+        mongodb = client.db('testCopy');
+    });
+}
+
+connectMongo();
 var getWorklog = function(key, auth, cb){
     var url = 'https://issues.qup.vn/rest/api/2/issue/'+key+'/worklog';
-    // getCacheRedis(url, function(err, data) {
-        // if (!err && data) {
-            // cb(err, data);
-        // } else {
+    getCacheMongo(key, function(err, data) {
+        if (!err && data) {
+            cb(err, data);
+        } else {
             request({
                 url: url,
                 timeout: 10000,
@@ -24,41 +39,75 @@ var getWorklog = function(key, auth, cb){
                         }
                     }
                     var rs = _.map(result.worklogs, filter);
-                    // setCacheRedis(url, rs, 60 * 60 * 30, function(err, ok){})
+                    setCacheMongo(key, rs, function(err, ok){})
                     cb(null, rs);
                 } else {
+                    setCacheMongo(key, [], function(err, ok){})
                     cb("notfound", [])
                 }
-            })
-        // }
-    // })   
-}
-function getCacheRedis(key, cb){
-    redis.get(key, function(err, data){
-        if (err) {
-            console.log('getCacheRedis error');
-            console.log(err);
-            cb(err, data);
-        } else {
-            try {
-                cb(null, JSON.parse(data));
-            } catch (ex) {
-                console.log('getCacheRedis error');
-                console.log(ex);
-                cb(ex, null)
+            });
+        }
+    });   
+};
+var setResults = function(sql, data){
+    if (mongodb) {
+        var bindData = {
+            time: new Date(),
+            sql: sql,
+            data: JSON.stringify(data)
+        };
+        var JiraResults = mongodb.collection('JiraResults');
+        JiraResults.insertOne(bindData);
+    } else {
+        console.log("mongodb error", error);
+    }
+}; 
+var getResults = function(sql, cb){
+    if (mongodb) {
+        var JiraResults = mongodb.collection('JiraResults');
+        JiraResults.findOne({
+            sql: sql
+        }, function(err, data){
+            if (!err && data) {
+                cb(null, data);
+            } else {
+                cb("mongodb error", null);
             }
-        }
-    })
+        });
+    } else {
+        cb("mongodb error", null);
+    }
+}; 
+function getCacheMongo(key, cb){
+    if (mongodb) {
+        var JiraLogworks = mongodb.collection('JiraLogworks');
+        JiraLogworks.findOne({
+            key: key
+        }, function(err, data){
+            if (!err && data && data.data) {
+                cb(null, data.data);
+            } else {
+                cb("mongodb error", null);
+            }
+        });
+    } else {
+        cb("mongodb error", null);
+    }
 }
-function setCacheRedis(key, data, time, cb){
-    redis.setex(key, time, JSON.stringify(data), function(err, ok){
-        if (err) {
-            console.log('setCacheRedis error');
-            console.log(err);
-        }
-        cb(err, ok);
-    })
+function setCacheMongo(key, data, cb){
+    if (mongodb) {
+        var JiraLogworks = mongodb.collection('JiraLogworks');
+        JiraLogworks.insertOne({
+            time: new Date(),
+            key: key,
+            data: data
+        }, cb);
+    } else {
+        cb("mongodb error", null);
+    }
 }
 module.exports = {
-    getWorklog
+    getWorklog,
+    setResults,
+    getResults
 }
