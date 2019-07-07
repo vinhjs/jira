e5707f063e755b8b04f2ed980423c9740fafeb94
vinhjs <vinh.tran@qupworld.com-at-497475774578>
2019-07-07 21:42:21 +0700

    setting

---
+++ config/redis.js
@@ -0,0 +1,23 @@
+var redis = require("redis");
+var config = require('config');
+var port = config.get('redis.port');
+var server = config.get('redis.server');
+var pw = config.get('redis.password');
+
+
+var redisClient = redis.createClient({
+    host: server,
+    port: port,
+    password: pw
+});
+redisClient.on('error', function (err) {
+    console.log(new Date().toISOString()+ ' redisSetting connection error to - ' + err);
+    process.exit(1);
+});
+redisClient.on('ready', function (err) {
+    console.log(new Date().toISOString()+ ' redisSetting connection success - ' + err);
+});
+
+module.exports = {
+    redisClient : redisClient
+};
